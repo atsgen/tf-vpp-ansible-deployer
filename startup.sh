1edf2f5309859fbc14119ce54c62c74bf4fd8d4f
Prabhjot Singh Sethi <prabhjot@atsgen.com>
2019-08-29 11:49:38 +0530

    fix usuage of yum for centos
    Signed-off-by: Prabhjot Singh Sethi <prabhjot@atsgen.com>

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 TAG=${TAG:-v5.1}
 
-apt-get install -y wget epel-release python-pip ansible-2.4.2.0
+yum install -y wget epel-release python-pip ansible-2.4.2.0
 
 wget http://134.119.178.86:10090/centos/3.10.0-862.3.2.el7.x86_64/igb_uio.ko
 modprobe uio && insmod ./igb_uio.ko
