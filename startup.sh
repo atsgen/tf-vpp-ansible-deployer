dab0f88075176172ac0c8fb3a1b5124aa72d10c3
Prabhjot Singh Sethi <prabhjot@atsgen.com>
2019-08-30 22:31:45 +0530

    fix startup script
    Signed-off-by: Prabhjot Singh Sethi <prabhjot@atsgen.com>

--- startup.sh
+++ startup.sh
@@ -2,10 +2,15 @@
 
 TAG=${TAG:-v5.1}
 
-yum install -y wget epel-release python-pip ansible-2.4.2.0
+yum install -y wget epel-release
 
-wget http://134.119.178.86:10090/centos/3.10.0-862.3.2.el7.x86_64/igb_uio.ko
-modprobe uio && insmod ./igb_uio.ko
+yum install -y python-pip
+
+pip install requests
+pip install ansible==2.5.2.0
+
+modprobe uio
+wget http://134.119.178.86:10090/centos/`uname -r`/igb_uio.ko && insmod ./igb_uio.ko
 
 git clone -b $TAG https://github.com/Juniper/contrail-ansible-deployer.git
 
