7af4e6e9510d362354003551e81a182f9c5dae79
Prabhjot Singh Sethi <prabhjot@atsgen.com>
2019-09-13 19:17:57 +0000

    fix tag for correct kubernetes version
    Signed-off-by: Prabhjot Singh Sethi <prabhjot@atsgen.com>

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-TAG=${TAG:-v5.1}
+#TAG=${TAG:-v5.1}
+TAG=${TAG:-9173bb48895428dc35848d270fada5d47f920476}
 
 yum install -y wget epel-release
 
@@ -12,7 +13,8 @@ pip install ansible==2.5.2.0
 modprobe uio
 wget http://134.119.178.86:10090/centos/`uname -r`/igb_uio.ko && insmod ./igb_uio.ko
 
-git clone -b $TAG https://github.com/Juniper/contrail-ansible-deployer.git
+git clone https://github.com/Juniper/contrail-ansible-deployer.git
+(cd contrail-ansible-deployer && git checkout $TAG)
 
 scp -r playbooks/install_vpp.yml contrail-ansible-deployer/playbooks/.
 scp -r playbooks/roles/* contrail-ansible-deployer/playbooks/roles/.
