f2867edbc8a91310fc0a374ea049ccd0092fd6d6
Prabhjot Singh Sethi <prabhjot@atsgen.com>
2019-11-20 07:48:11 +0100

    initial commit for tungsten R5.1
    Signed-off-by: Prabhjot Singh Sethi <prabhjot@atsgen.com>

--- startup.sh
+++ startup.sh
@@ -13,8 +13,11 @@ pip install ansible==2.5.2.0
 modprobe uio
 wget http://134.119.178.86:10090/centos/`uname -r`/igb_uio.ko && insmod ./igb_uio.ko
 
-git clone https://github.com/Juniper/contrail-ansible-deployer.git
-(cd contrail-ansible-deployer && git checkout $TAG)
+git clone https://github.com/atsgen/contrail-ansible-deployer.git
+(cd contrail-ansible-deployer && git checkout tungsten-R5.1)
+
+git clone https://github.com/atsgen/contrail-kolla-ansible.git
+(cd contrail-kolla-ansible && git checkout e425109499170ac5fbcd0b83cb3b9829db1b1a77)
 
 scp -r playbooks/install_vpp.yml contrail-ansible-deployer/playbooks/.
 scp -r playbooks/roles/* contrail-ansible-deployer/playbooks/roles/.
