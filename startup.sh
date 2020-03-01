9812f9cfeda2cbb2fefca601d7993464f4f753b7
Prabhjot Singh Sethi <prabhjot@atsgen.com>
2020-03-01 22:24:02 +0530

    add support for ubuntu
    Signed-off-by: Prabhjot Singh Sethi <prabhjot@atsgen.com>

--- startup.sh
+++ startup.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+source $(dirname $0)/utils/functions.sh
+
 #TAG=${TAG:-v5.1}
 TAG=${TAG:-9173bb48895428dc35848d270fada5d47f920476}
 
-yum install -y wget epel-release
+if is_centos; then
+  yum install -y epel-release
+  yum install -y python-pip
+else
+  if is_ubuntu; then
+    apt-get install -y python-pip
+  else
+    echo "UnSupported OS ..."
+    exit 1
+  fi
+fi
 
-yum install -y python-pip
 
 pip install requests
 pip install ansible==2.5.2.0
 
-modprobe uio
-wget http://134.119.178.86:10090/centos/`uname -r`/igb_uio.ko && insmod ./igb_uio.ko
+#modprobe uio
+#wget http://134.119.178.86:10090/centos/`uname -r`/igb_uio.ko && insmod ./igb_uio.ko
 
 git clone https://github.com/Juniper/contrail-ansible-deployer.git
 (cd contrail-ansible-deployer && git checkout $TAG)
