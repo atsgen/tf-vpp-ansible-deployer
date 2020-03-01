61cb5a6ecf8b01cf9fad0b7460c6434b25b242ad
Prabhjot Singh Sethi <prabhjot@atsgen.com>
2020-03-01 23:19:55 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -24,7 +24,7 @@ pip install ansible==2.5.2.0
 #modprobe uio
 #wget http://134.119.178.86:10090/centos/`uname -r`/igb_uio.ko && insmod ./igb_uio.ko
 
-git clone https://github.com/Juniper/contrail-ansible-deployer.git
+git clone https://github.com/atsgen/contrail-ansible-deployer.git
 (cd contrail-ansible-deployer && git checkout $TAG)
 
 scp -r playbooks/install_vpp.yml contrail-ansible-deployer/playbooks/.
